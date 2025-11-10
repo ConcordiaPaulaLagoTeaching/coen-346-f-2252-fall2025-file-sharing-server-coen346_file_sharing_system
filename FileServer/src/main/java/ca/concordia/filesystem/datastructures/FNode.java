f4f12d8b66279025262211bd2b79b8f828bca689
raghdaelkei <118774987+raghdaelkei@users.noreply.github.com>
2025-11-10 15:17:34 -0500

    implemented standalone file system layer(simulates disk , provides file operations and handles concurrency with locks) + some test files to test FileSystemManager.java

--- FileServer/src/main/java/ca/concordia/filesystem/datastructures/FNode.java
+++ FileServer/src/main/java/ca/concordia/filesystem/datastructures/FNode.java
@@ -9,4 +9,22 @@ public class FNode {
         this.blockIndex = blockIndex;
         this.next = -1;
     }
+
+    // Added minimal getters and setters used by FileSystemManager
+    public int getBlockIndex() {
+        return blockIndex;
+    }
+
+    public void setBlockIndex(int blockIndex) {
+        this.blockIndex = blockIndex;
+    }
+
+    public int getNext() {
+        return next;
+    }
+
+    public void setNext(int next) {
+        this.next = next;
+    }
 }
+
