4f86c492f605dca4b265828650058cc9a8acbbc8
Prathiksha Kandiah <prathiksha.kandiah@gmail.com>
2025-11-12 19:50:35 -0500

    Added ListCommandHandler class and integrated LIST command in FileServer

--- FileServer/src/main/java/ca/concordia/filesystem/FileSystemManager.java
+++ FileServer/src/main/java/ca/concordia/filesystem/FileSystemManager.java
@@ -3,13 +3,14 @@ package ca.concordia.filesystem;
 import ca.concordia.filesystem.datastructures.FEntry;
 
 import java.io.RandomAccessFile;
+import java.io.IOException;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class FileSystemManager {
 
-    private final int MAXFILES = 5;
+	private final int MAXFILES = 5;
     private final int MAXBLOCKS = 10;
-    private final static FileSystemManager instance;
+    private static FileSystemManager instance;
     private final RandomAccessFile disk;
     private final ReentrantLock globalLock = new ReentrantLock();
 
@@ -18,21 +19,27 @@ public class FileSystemManager {
     private FEntry[] inodeTable; // Array of inodes
     private boolean[] freeBlockList; // Bitmap for free blocks
 
-    public FileSystemManager(String filename, int totalSize) {
+    public static synchronized FileSystemManager getInstance (String filename, int totalSize) throws IOException {
         // Initialize the file system manager with a file
         if(instance == null) {
-            //TODO Initialize the file system
-        } else {
-            throw new IllegalStateException("FileSystemManager is already initialized.");
-        }
+        	instance = new FileSystemManager(filename, totalSize);
+        } 
+        return instance;
+    }
 
+
+
+    private FileSystemManager(String filename, int totalSize) throws IOException {
+
+        this.disk = new RandomAccessFile(filename, "rw");
+        this.inodeTable = new FEntry[MAXFILES];
+        this.freeBlockList = new boolean[MAXBLOCKS];
     }
 
     public void createFile(String fileName) throws Exception {
-        // TODO
+
         throw new UnsupportedOperationException("Method not implemented yet.");
     }
 
 
-    // TODO: Add readFile, writeFile and other required methods,
 }
