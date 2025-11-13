b583798a6ec2ed566831de1a8b9e9316f349d9a8
Prathiksha Kandiah <prathiksha.kandiah@gmail.com>
2025-11-13 11:24:17 -0500

    step1:filesystem implementation

--- FileServer/src/main/java/ca/concordia/filesystem/datastructures/FEntry.java
+++ FileServer/src/main/java/ca/concordia/filesystem/datastructures/FEntry.java
@@ -4,44 +4,55 @@ import java.util.LinkedList;
 
 public class FEntry {
 
-    private String filename;
+    private String name;
     private short filesize;
-    private short firstBlock; // Pointers to data blocks
+    private short firstBlock;
+    private byte[] data;
 
-    public FEntry(String filename, short filesize, short firstblock) throws IllegalArgumentException{
+    public FEntry(String name, byte[] data){
         //Check filename is max 11 bytes long
-        if (filename.length() > 11) {
+        if (name.length() > 11) {
             throw new IllegalArgumentException("Filename cannot be longer than 11 characters.");
         }
-        this.filename = filename;
-        this.filesize = filesize;
-        this.firstBlock = firstblock;
+        this.name = name;
+        this.data = data;
+        this.filesize = (short) data.length;
+        this.firstBlock = -1;
     }
 
     // Getters and Setters
-    public String getFilename() {
-        return filename;
+    public String geteName() {
+        return name;
     }
-
-    public void setFilename(String filename) {
-        if (filename.length() > 11) {
-            throw new IllegalArgumentException("Filename cannot be longer than 11 characters.");
-        }
-        this.filename = filename;
+    
+    public byte[] getData() {
+    	return data;
     }
 
     public short getFilesize() {
         return filesize;
     }
 
+
+    public short getFirstBlock() {
+        return firstBlock;
+    }
+    
+    
+    public void setName(String name) {
+	  if (name.length() > 11) {
+		  throw new IllegalArgumentException("Filename cannot be longer than 11 characters.");
+      }
+	  this.name = name;
+	  }
+    
     public void setFilesize(short filesize) {
         if (filesize < 0) {
             throw new IllegalArgumentException("Filesize cannot be negative.");
         }
         this.filesize = filesize;
     }
+  
+  }
 
-    public short getFirstBlock() {
-        return firstBlock;
-    }
-}
+
